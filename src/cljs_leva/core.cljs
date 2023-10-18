20477bcf99b68a7e8dc7edfd9f708c4c5efa41d9
Liam Duffy <liamdanielduffy@gmail.com>
2023-10-18 16:34:36 -0400

    initialize divs with different styles

--- src/cljs_leva/core.cljs
+++ src/cljs_leva/core.cljs
@@ -16,13 +16,8 @@
 (defn selected [id]
   (= @selected-id id))
 
-(defn toggle-leva-on-click []
-  (reagent/with-let [!styles (reagent/atom {:background-color "orange"
-                                            :height "100px"
-                                            :width "100px"
-                                            :border-color "black"
-                                            :border-style "solid"
-                                            :border-width "1px"})
+(defn toggle-leva-on-click [initial-styles]
+  (reagent/with-let [!styles (reagent/atom initial-styles)
                      id (str (random-uuid))]
     [:div
      [:div {:style @!styles :on-click #(select id)}]
@@ -30,7 +25,19 @@
        (leva-panel !styles))]))
 
 (defn mount-root []
-  (d/render [:div [toggle-leva-on-click] [toggle-leva-on-click]] (.getElementById js/document "app")))
+  (d/render
+   [:div
+    [toggle-leva-on-click {:background-color "orange"
+                           :height "100px"
+                           :width "100px"
+                           :border-color "black"
+                           :border-style "solid"
+                           :border-width "1px"}]
+    [toggle-leva-on-click {:background-color "green"
+                           :height "100px"
+                           :width "100px"
+                           :border-radius "30px"}]]
+   (.getElementById js/document "app")))
 
 (defn ^:export init! []
   (mount-root))
